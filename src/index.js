8c36a07d81be2bbfb83746eb324a37f409dff5b2
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-13 17:43:23 -0500

    added 2 sagas and changed reducers to if statement

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
+import axios from 'axios';
 
 // Saga setup
 import createSagaMiddleware from 'redux-saga';
@@ -15,31 +16,61 @@ const sagaMiddleware = createSagaMiddleware();
 
 // ------ WATCHER SAGA -------
 function* watcherSaga() {
+  yield takeEvery( 'GET_ALL_PROJECTS', getProjectsList );
+  yield takeEvery( 'DELETE_PROJECT', deleteProject );
 }
 
 // ------ SAGAS -------
 
-
-// ------ REDUCERS -------
-
-// Used to store projects returned from the server
-const projects = (state = [], action) => {
-  switch (action.type) {
-    case 'SET_PROJECTS':
-      return action.payload;
-    default:
-      return state;
+// GET
+function* getProjectsList( action ) {
+  console.log('Hit the getProjectsList saga', action);
+  try {
+    const getResponse = yield axios.get('/projects');
+    console.log(getResponse);
+    const action = { type: 'SET_PROJECTS', payload: getResponse.data };
+    // Dispatch an action to our reducers to update our redux store
+    yield put( action );
+  }
+  catch (error) {
+    console.log(`Couldn't get projects`, error);
+    alert(`Sorry, couldn't get the projects. Try again later`);
   }
 }
 
-// Used to store the project tags (e.g. 'React', 'jQuery', 'Angular', 'Node.js')
-const tags = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_TAGS':
-            return action.payload;
-        default:
-            return state;
-    }
+// DELETE
+function* deleteProject( action ) {
+  // action.payload should be an number that represents a project id
+  // that matches a project id in the database
+  console.log('hit the DELETE project saga', action);
+  console.log('action.payload:', action.payload);
+  try {
+    yield axios.delete(`/projects/${action.payload}`)
+    // reassign saga action to reducer action
+    action = { type: 'GET_PLANTS' };
+    yield put( action );
+  } catch (error) {
+    console.log(`Couldn't delete plant`, error);
+    alert(`Sorry, couldn't delete the plant. Try again later`);
+  }
+}
+
+// ------ REDUCERS -------
+
+// all projects returned from the server
+const projects = ( state = [], action ) => {
+  if (action.type === 'SET_PROJECTS') {
+    return action.payload;
+  }
+  return state;
+}
+
+// the project tags (e.g. 'React', 'jQuery', 'Angular', 'Node.js')
+const tags = ( state = [], action ) => {
+  if (action.type === 'SET_TAGS') {
+    return action.payload;
+  }
+  return state;
 }
 
 // Create one store that all components can use
@@ -49,7 +80,7 @@ const storeInstance = createStore(
         tags,
     }),
     // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+    applyMiddleware( sagaMiddleware, logger ),
 );
 
 // This tells the saga middleware to run the watcherSaga 
