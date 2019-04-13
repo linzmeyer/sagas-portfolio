62ca3762c9eab8e28d582a1d9851c998d00a0fef
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-13 09:00:57 -0500

    added more project setup | new files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
+// import all components used in this component's render
+import Admin from '../Admin/Admin';
+import Home from '../Home/Home';
+
 class App extends Component {
-  // Renders the entire app on the DOM
+
   render() {
     return (
-      <div className="App">
-        <p>Empty Page</p>
-      </div>
+      // This gives these components react routes for client side routing and SPA functionality.
+      <Router>
+      	<Route exact path='/admin' component={ Admin } />
+				<Route exact path='/' component={ Home }/>
+      </Router>
     );
   }
 }
