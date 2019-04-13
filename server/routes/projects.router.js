4e351918730ae85ae58adf56208fdf7b09f8a0a4
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-13 17:42:35 -0500

    init | added GET | SELECT * FROM projects

---
+++ server/routes/projects.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+// GET all projects
+router.get('/', (req, res) => {
+  const queryText = 'SELECT * FROM projects';
+  pool.query(queryText)
+    .then((result) => { res.send(result.rows); })
+    .catch((err) => {
+      console.log('Error completing SELECT projects query', err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
