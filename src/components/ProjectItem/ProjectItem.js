b9d7fefe132d9654e3f5bb47aa3e43833a8b14e3
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-14 12:36:44 -0500

    minor refactor (spacing)

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+
 class ProjectItem extends Component {
 
   render () {
     return (
 			<div>
-				<p>Poject Name:  {this.props.project.name}</p>
-				<p>Poject Description:  {this.props.project.description}</p>
-				<p>Poject Thumbnail:  {this.props.project.thumbnail}</p>
-				<p>Poject Website:  {this.props.project.website}</p>
-				<p>Poject Github: <a href={this.props.project.github}>Repo</a></p>
+				<img alt='...'/>
+				<p>{ this.props.project.name }</p>
+				<p>Poject Description:  { this.props.project.description }</p>
+				<p>Poject Website:  { this.props.project.website }</p>
+				<p>Poject Github: <a href={ this.props.project.github }>Repo</a></p>
 				<p>Poject Date Completed:  { this.props.project.date_completed }</p>
-				<p>Poject Technology Used:  {this.props.project.name}</p>
+				<p>Poject Technology Used:  { this.props.project.name }</p>
 			</div>
 		);
   }
 }
 
-export default(ProjectItem);
+export default( ProjectItem );
