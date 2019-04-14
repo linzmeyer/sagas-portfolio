38d717c78a59ca6166a80371d9cb054ad1785c7b
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-13 19:43:18 -0500

    added ProjectsList component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,15 +6,18 @@ import './App.css';
 // import all components used in this component's render
 import Admin from '../Admin/Admin';
 import Home from '../Home/Home';
+import ProjectsList from '../ProjectsList/ProjectsList';
 
 class App extends Component {
 
   render() {
     return (
-      // This gives these components react routes for client side routing and SPA functionality.
+      // This gives these components react routes for client side routing and 
+      // SPA functionality.
       <Router>
       	<Route exact path='/admin' component={ Admin } />
-				<Route exact path='/' component={ Home }/>
+				<Route exact path='/' component={ Home } />
+				<Route exact path='/projects' component={ ProjectsList }/>
       </Router>
     );
   }
