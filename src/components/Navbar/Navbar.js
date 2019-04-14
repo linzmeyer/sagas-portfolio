6cd273693f064deb1527c03055f0070ebd3ed455
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-13 19:40:16 -0500

    nav ba rendering based on url pathname

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// This component is not a direct child of a <Route />,
+// so withRouter is required to use this.props.history.push() for react routing
+import { withRouter } from 'react-router-dom';
+
 class Navbar extends Component {
 
   	// Click handlers that route the user
 	routeToAdmin = () => { this.props.history.push( '/admin' ) }
 	routeToHome = () => { this.props.history.push( '/' ) }
-	routeToView1 = () => { this.props.history.push( '/projects' ) }
+	routeToProjects = () => { this.props.history.push( '/projects' ) }
 
-  // try to conditionally render using current view
-  // find current view by using this.props.history...
-  renderNavBar = ( view ) => {
-    if (view === 'Admin') {
-      <p>Hello Navbar For Admin</p>
+  // 1. if current view is admin
+  // 2. if current view is home
+  // 3. if current view is projects
+  renderNavigation = ( view ) => {
+    if (view === '/admin') {
+      return(
+        <div>
+          <p>Hello Navbar For Admin</p>
+
+        </div>
+
+      );
     }
-    else if (view === 'Home') {
-      <p>Hello Navbar For Home</p>
+    else if (view === '/') {
+      return (
+        <p>Hello Navbar For Home</p>
+      );
     }
-    else if (view === 'Projects') {
-      <p>Hello Navbar For Projects</p>
+    else if (view === '/projects') {
+      return (
+        <p>Hello Navbar For Projects</p>
+      );
     }
   }
 
   render() {
     return (
       <nav>
-        { this.renderNavBar( this.props.history ) }
+        { this.renderNavigation( this.props.location.pathname ) }
       </nav>
     );
   }
 }
 
-const mapReduxStateToProps = reduxState => ({
-  reduxState
-})
-
-export default connect(mapReduxStateToProps)(Navbar);
+// This uses connect for redux and withRouter for react routing.
+export default connect()(withRouter( Navbar ));
