4507ef82d3f8138de2f0df7ddab8288fc78d6db5
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-14 00:45:23 -0500

    moved a bunch of unnecessaru stuff

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-class Plant extends Component {
+class ProjectItem extends Component {
 
-	deleteProject = () => {
-		this.props.dispatch( { type: 'DELETE_PROJECT', payload: this.props.project.id } );
-  }
-  
   render () {
     return (
 			<div>
 				<p>Poject Name:  {this.props.project.name}</p>
-  			<button onClick={this.deleteProject}>Delete</button >
 			</div>
 		);
   }
 }
 
-export default connect()(Plant);
+export default(ProjectItem);
