f58b6cb5f00d32e75343186df4fa201b52e0d318
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-14 00:33:50 -0500

    it should conditionally render header based on URL

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
-	render() {
+import Navbar from '../Navbar/Navbar';
 
+// need this to access this.props.location for conditional rendering
+import { withRouter } from 'react-router-dom';
+
+class Header extends Component {
+
+	renderHeader = ( currentView ) => {
+			// if current view is admin
+			if (currentView === '/admin') {
+				return( <h1>ADMIN</h1> );
+			}
+			// if current view is home
+			else if (currentView === '/') {
+				return( <h1>HOME</h1> );
+			} // if current view is projects
+			else if (currentView === '/projects') {
+				return( <h1>PROJECTS</h1> );
+			}
+		}
+
+	render() {
+		return (
+			<div>
+				{ this.renderHeader( this.props.location.pathname ) }
+				<Navbar />
+			</div>
+		);
 	}
 }
 
-export default( Header );
+// This uses withRouter for access to props.location.
+export default( withRouter( Header ));
