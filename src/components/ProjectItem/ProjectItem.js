e343271b1f626ad3e7229bd73232b5024193ccb9
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-14 01:20:49 -0500

    getting properties of object back!

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -5,6 +5,12 @@ class ProjectItem extends Component {
     return (
 			<div>
 				<p>Poject Name:  {this.props.project.name}</p>
+				<p>Poject Description:  {this.props.project.description}</p>
+				<p>Poject Thumbnail:  {this.props.project.thumbnail}</p>
+				<p>Poject Website:  {this.props.project.website}</p>
+				<p>Poject Github: <a href={this.props.project.github}>Repo</a></p>
+				<p>Poject Date Completed:  { this.props.project.date_completed }</p>
+				<p>Poject Technology Used:  {this.props.project.name}</p>
 			</div>
 		);
   }
