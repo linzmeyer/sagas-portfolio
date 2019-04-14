74de30123a356adcb2f70b64594588ecd7ae2fef
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-14 00:45:39 -0500

    init

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Header from '../Header/Header';
+import Navbar from '../Navbar/Navbar';
+import ProjectsList from '../ProjectsList/ProjectsList';
+
+// Parent: <Route /> in <App />
+class Admin extends Component {
+	render() {
+    return (
+      <div>
+        <Header />
+        <ProjectsList />
+        <Navbar />
+      </div>
+    );
+	}
+}
+
+export default( Admin );
