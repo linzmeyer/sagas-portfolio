fcdf9c34affc818854d80605ac400f17c9de16cb
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-04-13 19:43:37 -0500

    added Navbar component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from '../Navbar/Navbar';
+
 
 class Home extends Component {
 	render() {
     return (
-      <h2>Hello Home</h2>
+      <div>
+        <h2>Hello Home</h2>
+        <Navbar />
+      </div>
     );
 	}
 }
